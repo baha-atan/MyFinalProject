0053610644a14f3be5066fc978b1d5748bd05042
Baha ATAN <bahaatan.27@gmail.com>
2022-08-10 10:54:48 +0300

    Contiune

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,4 +1,6 @@
 ﻿using Business.Abstract;
+using Business.Constants;
+using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
 using Entities.DTOs;
@@ -17,6 +19,16 @@ namespace Business.Concrete
 			_productDal = productDal;
 		}
 
+		public IResult Add(Product product)
+		{
+			if (product.ProductName.Length<2)
+			{
+				return new ErrorResult(Messages.ProductNameInvalid);
+			}
+			_productDal.Add(product);
+			return new SuccessResult(Messages.ProductAdded);
+		}
+
 		public List<Product> GetAll()
 		{
 			return _productDal.GetAll();
@@ -25,6 +37,11 @@ namespace Business.Concrete
 		public List<Product> GetAllByCategoryId(int id)
 		{
 			return _productDal.GetAll(p => p.CategoryID == id);
+		}
+
+		public Product GetById(int productId)
+		{
+			return _productDal.Get(p => p.ProductID == productId);
 		}
 
 		public List<Product> GetByUnitsPrice(decimal min, decimal max)
